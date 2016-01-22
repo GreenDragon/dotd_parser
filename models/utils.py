9f16045b5f0499cd4d93c88bc9fb44ed5a90cdf5
GreenDragon <elf-github@arashi.com>
2016-01-22 02:43:55 -0800

    V1.8.2 LoTS Language Fixes, Armament Updates

---
+++ models/utils.py
@@ -0,0 +1,23 @@
+import urllib
+
+def gen_wiki_url(proc_name, suns_mode):
+    dotd_wiki = 'http://dotd.wikia.com/wiki/Special:Search?search='
+    lots_wiki = 'http://zoywiki.com/index.php?search=LotS+'
+    if suns_mode:
+        return lots_wiki + str(urllib.quote_plus(proc_name)) + '&button=&title=Special%3ASearch'
+    else:
+        return dotd_wiki + str(urllib.quote_plus(proc_name)) + '&fulltext=Search'
+
+
+def monify(suns_mode):
+    if suns_mode:
+        return "Credits"
+    else:
+        return "Gold"
+
+
+def magify(suns_mode):
+    if suns_mode:
+        return "Tactics"
+    else:
+        return "Magics"
